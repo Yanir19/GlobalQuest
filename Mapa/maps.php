7042831e5b5eb5e7cc693e68a0bf95723ec68e61
Yanir19 <yanir_jcl@hotmail.com>
2015-04-27 11:51:45 -0430

    Primera subida, incluye los archivos de bootstrap, 2 plantillas de encuestas y el API v3 de Goolge

---
+++ Mapa/maps.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    
+    </head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../Administrador/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../Administrador/css/bootstrap-theme.min.css" rel="stylesheet">
+  
+    
+    <title>Mapas</title>
+    
+    <div class="row">
+  <div class="col-md-8"> <style>
+   *{ margin: 0; padding: 0; }
+    html, body, #map{
+        width: 90%;
+        height: 90%;
+    }
+    </style>
+    </div>
+        
+  <div class="col-md-4">.col-md-4</div>
+    
+  
+  
+  
+</div>
+   
+<body>
+    <div id="map"></div>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&amp;language=es"></script>
+    <script type="text/javascript" src="map.js"></script>
+    <script src="../Administrador/js/jquery.js"></script>
+    <script src="../Administrador/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+  <body>
+      <header>
+          <div class="container">
+              <h1>Plantilla #1</h1>
+          </div>
+      </header>
+      
+       <div class="container">
+           <div class="col-lg-6">
+           <div class="panel panel-danger">
+                <div class="panel-heading">Seleccion simple</div>
+                   <div class="panel-body">
+                       
+                        <ul class="list-group">
+                   <li class="list-group-item"> <div class="radio">
+                        <label><input type="radio" name="optradio">Option 1</label>
+                      </div></li>
+                   <li class="list-group-item"><div class="radio">
+                        <label><input type="radio" name="optradio">Option 2</label>
+                      </div></li>
+                   <li class="list-group-item"><div class="radio disabled">
+                        <label><input type="radio" name="optradio" disabled>Option 3</label>
+                      </div></li>
+                </ul>
+                       <button type="button" class="btn btn-danger">Siguiente</button>
+                </div>
+               
+             </div>
+        </div>       
+       </div>
+      <section class="main">
+          
+      </section>
+    
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
